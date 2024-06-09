7469b9135b05697cc251d77ea64a0526c8631773
Gowdhamraman <159995194+Gowdhamraman@users.noreply.github.com>
2024-06-09 15:42:50 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ sudo yum update -y
 sudo yum install -y git docker
 
 # Install Java (required for Jenkins)
-sudo yum install -y java-11-openjdk-devel
+sudo yum install java-17-amazon-corretto -y
 
 # Add Jenkins repository and import the GPG key
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
