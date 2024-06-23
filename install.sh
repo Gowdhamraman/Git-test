f16a05c51f72d102bbea5534053af5ad16339d76
Gowdhamraman <159995194+Gowdhamraman@users.noreply.github.com>
2024-06-23 12:57:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ sudo yum install -y git docker
 sudo yum install java-17-amazon-corretto -y
 
 # Add Jenkins repository and import the GPG key
-sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo wget -O /etc/yum.repos.d/jenkins.repo |   https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 
 # Install Jenkins
 sudo yum install -y jenkins
